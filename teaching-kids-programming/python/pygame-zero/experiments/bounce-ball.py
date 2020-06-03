3473d95a505b80e616110042a722c8bec4f7eb02
Peter Marklund <peter.marklund@schibsted.com>
2020-06-03 10:50:27 +0200

    added score to bouncing ball game

--- teaching-kids-programming/python/pygame-zero/experiments/bounce-ball.py
+++ teaching-kids-programming/python/pygame-zero/experiments/bounce-ball.py
@@ -6,21 +6,25 @@ TITLE = 'Bounce Ball'
 WIDTH = 800
 HEIGHT = 600
 
-game_over = False
-gravity = 1
-control = 1
+def init_game():
+  global ball, obstacles, score, game_over, gravity, control
+  ball = make_ball()
+  obstacles = [make_obstacle() for _ in range(0, 2)]
+  game_over = False
+  score = 0
+  gravity = 1
+  control = 1
 
-ball = {
-  'x': WIDTH / 2,
-  'y': HEIGHT / 2,
-  'dx': 0,
-  'dy': 0,
-  'radius': randint(10, 20),
-  'color': 'red',
-  'slow': 3
-}
-
-print(ball)
+def make_ball():
+  return {
+    'x': WIDTH / 2,
+    'y': HEIGHT / 2,
+    'dx': 0,
+    'dy': 0,
+    'radius': randint(10, 20),
+    'color': 'red',
+    'slow': 3
+  }
 
 def make_obstacle():
   radius = randint(10, 30)
@@ -30,10 +34,6 @@ def make_obstacle():
     'radius': radius,
     'color': 'yellow'
   }
-
-obstacles = [make_obstacle() for _ in range(0, 2)]
-
-print(obstacles)
 
 def slow(velocity, amount):
   if velocity > 0:
@@ -48,19 +48,30 @@ def move_ball():
   ball['y'] += ball['dy']
   ball['dy'] += gravity
 
+def update_score(ball):
+  global score
+  # TODO: don't update score if last score was at the same position
+  if ball['dy'] > 0 or ball['dx'] > 0:
+    score += (HEIGHT - ball['radius'] - ball['y'])
+  print(score)
+
 def bounce_ball():
   if ball['x'] + ball['radius'] >= WIDTH: # bounce right wall
     ball['x'] = WIDTH - ball['radius']
     ball['dx'] = -1 * slow(ball['dx'], ball['slow'])
+    update_score(ball)
   if ball['x'] - ball['radius'] <= 0: # bounce left wall
     ball['x'] = ball['radius']
     ball['dx'] = -1 * slow(ball['dx'], ball['slow'])
+    update_score(ball)
   if ball['y'] - ball['radius'] <= 0: # bounce top wall
     ball['y'] = ball['radius']
     ball['dy'] = -1 * slow(ball['dy'], ball['slow'])
+    update_score(ball)
   if ball['y'] + ball['radius'] >= HEIGHT: # bounce bottom wall
     ball['y'] = HEIGHT - ball['radius']
     ball['dy'] = -1 * slow(ball['dy'], ball['slow'])
+    update_score(ball)
 
 def control_ball():
   if keyboard.up:
@@ -93,6 +104,8 @@ def ball_collision(ball1, ball2):
 
 def update():
   global game_over
+  if keyboard.space:
+    init_game()
   if game_over:
     return
   move_ball()
@@ -106,8 +119,11 @@ def draw():
     screen.draw.text(f"Game Over", (WIDTH/2,HEIGHT/2), color=(255,255,255), fontsize=30)
     return
   screen.fill('blue')
+  screen.draw.text(str(score), (5, 5), color=(255,255,255), fontsize=30)
   draw_ball(ball)
   for obstacle in obstacles:
     draw_ball(obstacle)
+
+init_game()
 
 pgzrun.go() # Must be last line
