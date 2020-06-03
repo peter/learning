635141f1bfd1cfafde77939bc2b6a75d97225322
Peter Marklund <peter.marklund@schibsted.com>
2020-06-03 13:50:06 +0200

    added enemies to galaga game

--- teaching-kids-programming/python/pygame-zero/galaga/main.py
+++ teaching-kids-programming/python/pygame-zero/galaga/main.py
@@ -1,17 +1,38 @@
 import pgzrun
 
-TITLE = 'Galaga'
-WIDTH = 800
-HEIGHT = 600
+config = {
+  'title': 'Galaga',
+  'width': 800,
+  'height': 600,
+  'n_enemies': 3,
+  'ship_move': 5,
+  'bullet_move': 10,
+  'enemy_move': 3
+}
 
-ship_move = 5
-bullet_move = 10
+TITLE = config['title']
+WIDTH = config['width']
+HEIGHT = config['height']
 
-ship = Actor('ship')
-ship.x = WIDTH/2
-ship.bottom = HEIGHT - 10
+def init_ship():
+  global ship, bullets
+  ship = Actor('ship')
+  ship.x = WIDTH/2
+  ship.bottom = HEIGHT - 10
+  bullets = []
 
-bullets = []
+def init_enemies():
+  global enemies
+  enemies = []
+  for n in range(0, config['n_enemies']):
+    imageName = 'enemy1'
+    x = WIDTH/2 + n * (getattr(images, imageName).get_width() + 5)
+    enemy = Actor(imageName, (x, 0))
+    enemies.append(enemy)
+
+def init_game():
+  init_ship()
+  init_enemies()
 
 def shoot():
   bullets.append(Actor('bullet', (ship.x, ship.y)))
@@ -22,18 +43,26 @@ def on_key_down(key):
 
 def update():
   if keyboard.left:
-    ship.x -= ship_move
+    ship.x -= config['ship_move']
   elif keyboard.right:
-    ship.x += ship_move
+    ship.x += config['ship_move']
+  for enemy in enemies:
+    enemy.y += config['enemy_move']
+  if enemies[0].y > HEIGHT:
+    init_enemies()
   for bullet in bullets:
-    bullet.y -= bullet_move
+    bullet.y -= config['bullet_move']
     if bullet.y < 0:
       bullets.remove(bullet)
 
 def draw():
   screen.clear()
+  for enemy in enemies:
+    enemy.draw()
   for bullet in bullets:
     bullet.draw()
   ship.draw()
+
+init_game()
 
 pgzrun.go()
