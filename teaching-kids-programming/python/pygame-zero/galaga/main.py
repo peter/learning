b37d9d74f1df5ebeb9a07bb0bdc73e3a8416e3e4
Peter Marklund <peter.marklund@schibsted.com>
2020-06-03 14:00:22 +0200

    galaga bullet/enemy collision

--- teaching-kids-programming/python/pygame-zero/galaga/main.py
+++ teaching-kids-programming/python/pygame-zero/galaga/main.py
@@ -31,6 +31,8 @@ def init_enemies():
     enemies.append(enemy)
 
 def init_game():
+  global score
+  score = 0
   init_ship()
   init_enemies()
 
@@ -42,21 +44,28 @@ def on_key_down(key):
     shoot()
 
 def update():
+  global score
   if keyboard.left:
     ship.x -= config['ship_move']
   elif keyboard.right:
     ship.x += config['ship_move']
-  for enemy in enemies:
-    enemy.y += config['enemy_move']
-  if enemies[0].y > HEIGHT:
-    init_enemies()
   for bullet in bullets:
     bullet.y -= config['bullet_move']
     if bullet.y < 0:
       bullets.remove(bullet)
+  for enemy in enemies:
+    enemy.y += config['enemy_move']
+    for bullet in bullets:
+      if enemy.colliderect(bullet):
+        score += 100
+        enemies.remove(enemy)
+        bullets.remove(bullet)
+  if not enemies or enemies[0].y > HEIGHT:
+    init_enemies()
 
 def draw():
   screen.clear()
+  screen.draw.text(str(score), (5, 5), color=(255,255,255), fontsize=30)
   for enemy in enemies:
     enemy.draw()
   for bullet in bullets:
