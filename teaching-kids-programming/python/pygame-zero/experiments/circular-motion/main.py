a49f76d509ab4cc490b60326a9ef7b2693124e6d
Peter Marklund <peter.marklund@schibsted.com>
2020-06-03 20:57:23 +0200

    debugging circular motion - calculating radius

--- teaching-kids-programming/python/pygame-zero/experiments/circular-motion/main.py
+++ teaching-kids-programming/python/pygame-zero/experiments/circular-motion/main.py
@@ -11,14 +11,15 @@ start_pos = (start_x, start_y)
 bird = SimpleNamespace(**{
   'angle': 0,
   'angle_update': 0.06,
-  'speed': 5,
+  'speed': 10,
   'last_dx': None,
   'last_dy': None,
   'max': {'x': start_x, 'y': start_y},
   'min': {'x': start_x, 'y': start_y},
   'actor': Actor('bird0', start_pos)
 })
-print(f'angle={bird.angle} speed={bird.speed} start_pos={start_pos}')
+print(f'angle={bird.angle} angle_update={bird.angle_update} speed={bird.speed} start_pos={start_pos}')
+print(f'speed/angle_update={bird.speed/bird.angle_update} (expected radius)')
 
 def sign(value):
   return 1 if value >= 0 else -1
@@ -27,11 +28,11 @@ def update():
   dx = math.cos(bird.angle) * bird.speed
   if bird.last_dx and sign(bird.last_dx) != sign(dx):
     print(f'dx inflection at x={bird.actor.x} y={bird.actor.y}')
-    print(f"x max={bird.max['x']} min={bird.min['x']} diameter={bird.max['x'] - bird.min['x']}")
+    print(f"x max={bird.max['x']} min={bird.min['x']} radius={(bird.max['x'] - bird.min['x'])/2}")
   dy = math.sin(bird.angle) * bird.speed
   if bird.last_dy and sign(bird.last_dy) != sign(dy):
     print(f'dy inflection at x={bird.actor.x} y={bird.actor.y}')
-    print(f"y max={bird.max['y']} min={bird.min['y']} diameter={bird.max['y'] - bird.min['y']}")
+    print(f"y max={bird.max['y']} min={bird.min['y']} radius={(bird.max['y'] - bird.min['y'])/2}")
   bird.actor.x += dx
   bird.actor.y += dy
   bird.last_dx = dx
