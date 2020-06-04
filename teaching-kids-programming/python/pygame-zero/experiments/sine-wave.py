b7b625926c0560e4c081008670e9b68be3acea76
Peter Marklund <peter.marklund@schibsted.com>
2020-06-04 17:38:56 +0200

    Adding sine waves

--- teaching-kids-programming/python/pygame-zero/experiments/sine-wave.py
+++ teaching-kids-programming/python/pygame-zero/experiments/sine-wave.py
@@ -9,16 +9,24 @@ config = {
   'amplitude': 100
 }
 
+wave_length = config['wave_length']
 frames = 0
 start_pos = (WIDTH/2, HEIGHT/2)
 positions = [start_pos]
 
+def sine_y(x_delta, wave_length):
+  return math.sin(2 * math.pi * x_delta/wave_length) * config['amplitude']
+
 def update():
-  global frames, positions
+  global frames, positions, wave_length
   frames += 1
   pos = positions[-1]
   x = (pos[0] + 3) % WIDTH
-  y = start_pos[1] + math.sin(2 * math.pi * (x - start_pos[0])/config['wave_length']) * config['amplitude']
+  # if (pos[0] < start_pos[0] and x >= start_pos[0]):
+  #   wave_length = wave_length * 2
+  #   print(f'wave_length={wave_length}')
+  x_delta = x - start_pos[0]
+  y = start_pos[1] + sine_y(x_delta, wave_length) + 0.5 * sine_y(x_delta, wave_length*2)
   positions.append((x, y))
 
 def draw():
