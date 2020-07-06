958bb04e513d22a671d9312789bee9b23ea1148e
Peter Marklund <peter.marklund@schibsted.com>
2020-07-06 13:49:52 +0200

    Strategies for sten-sax-pase.py

--- teaching-kids-programming/python/sten-sax-pase.py
+++ teaching-kids-programming/python/sten-sax-pase.py
@@ -2,6 +2,7 @@ import random
 from collections import defaultdict
 
 CONFIG = {
+  'strategy': 'cycle',
   'players': ['spelare', 'dator'],
   'choices': ["sten", "sax", "påse"],
   'points_to_win': 3
@@ -37,19 +38,47 @@ def update_points(choices):
         STATE['points'][player] += 1
       print(f"{player}: val={choice} poäng={STATE['points'][player]}")
 
+def random_strategy():
+  return random.choice(CONFIG['choices'])
+
+def cycle_strategy():
+  if 'last_choices' in STATE:
+    last_index = CONFIG['choices'].index(STATE['last_choices']['dator'])
+    new_index = (last_index + 1) % len(CONFIG['choices'])
+    print(f'last_index={last_index} new_index={new_index}')
+    return CONFIG['choices'][new_index]
+  else:
+    return random_strategy()
+
+def repeat_strategy():
+  if 'last_choices' in STATE:
+    return STATE['last_choices']['spelare']
+  else:
+    return random_strategy()
+
+STRATEGIES = {
+  'random': random_strategy,
+  'cycle': cycle_strategy,
+  'repeat': repeat_strategy
+}
+
+def computer_choice():
+  return STRATEGIES[CONFIG['strategy']]()
+
 def game_loop():
   while not get_winner(STATE['points'], CONFIG['points_to_win']):
     STATE['round'] += 1
     print(f'\nOmgång {STATE["round"]}')
     choices = {
       'spelare': input("sten sax påse? "),
-      'dator': random.choice(CONFIG['choices'])
+      'dator': computer_choice()
     }
     if not valid_choice(choices['spelare']):
       valid_choices = ", ".join(CONFIG['choices'])
       print(f'Ogiltigt val... Du måste välja ett av {valid_choices}')
       continue
     update_points(choices)
+    STATE['last_choices'] = choices
 
 def print_winner():
   if get_winner(STATE['points'], CONFIG['points_to_win']) == 'spelare':
